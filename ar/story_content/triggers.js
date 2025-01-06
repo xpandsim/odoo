0d9e3d08d9f23886d0f1f1c4cbc0b39fd98a28ce
xpandsim <xpandsim@gmail.com>
2025-01-06 11:29:11 +0400

    Add files via upload

---
+++ ar/story_content/triggers.js
@@ -0,0 +1,96 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5iGWMY0F7AF":
+        Script1();
+        break;
+      case "6WXVBgo0ylG":
+        Script2();
+        break;
+      case "5tsxJLpG48X":
+        Script3();
+        break;
+      case "656ldBOx8rt":
+        Script4();
+        break;
+      case "61jGZx0e16l":
+        Script5();
+        break;
+      case "60Iog2PEh1U":
+        Script6();
+        break;
+      case "6rQ5GiIjviL":
+        Script7();
+        break;
+      case "6E5Tj4Djhb3":
+        Script8();
+        break;
+      case "6kxhVVCxXLf":
+        Script9();
+        break;
+      case "5hTcx8uPhMi":
+        Script10();
+        break;
+      case "5aKh3OgY91l":
+        Script11();
+        break;
+      case "6iCmrt3JrgT":
+        Script12();
+        break;
+      case "6FyWlik4wNE":
+        Script13();
+        break;
+      case "6j9qUy4YjMb":
+        Script14();
+        break;
+      case "5UtLqdBWzrd":
+        Script15();
+        break;
+      case "6ZnTQGSU7J7":
+        Script16();
+        break;
+      case "5YfCSYHQKs0":
+        Script17();
+        break;
+      case "6WWRhxXD6LZ":
+        Script18();
+        break;
+      case "6ES3Qbn00wT":
+        Script19();
+        break;
+      case "5dMZLNTceyv":
+        Script20();
+        break;
+      case "6B0pYXdCj3N":
+        Script21();
+        break;
+      case "67Zz8agQtFv":
+        Script22();
+        break;
+      case "5Xz2b0MJjs9":
+        Script23();
+        break;
+      case "6YKIqCgqJXH":
+        Script24();
+        break;
+      case "5ekiIyfKfJZ":
+        Script25();
+        break;
+      case "6TjbXhemK1m":
+        Script26();
+        break;
+      case "6IQqfhS7McZ":
+        Script27();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
